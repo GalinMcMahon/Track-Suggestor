6d621a045c78963657e22316b3a7835072742f0a
Galin McMahon <galinmcmahon@gmail.com>
2017-03-24 14:04:48 -0700

    make first question's js work

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,64 @@
 $(document).ready(function(event){
-    $(".goButton").click(function(){
+
+    $(".goButton").click(function(event){
       $(".monitor").hide();
       $(".goButton").hide();
-      $(".Questions").show(2000);
-
-  event.preventDefault();
+      $(".Questions").show(1000);
   });
+
+  var total = 0;
+  $(".enterButton").click(function(event) {
+      $("#Question1").fadeToggle(750);
+
+      var valueQ1 = parseInt($("#Q1").val());
+      total += valueQ1
+        alert(total);
+
 });
 
-    // $("#go").submit(function(event) {
+//   $(".enterButton2").click(function(event) {
+//         $("#Q2").show(1000);
+//       $("#Question2").fadeToggle(750);
+//
+//       var valueQ2 = parseInt($("#Q2").val());
+//       total += valueQ2
+// });
 
-        $("#Questions").fadeToggle(750);
-        var valueQ1 = parseInt($("#Q1").val());
-        total += valueQ1
-            alert(total);
-    //
-        event.preventDefault();
+
+      event.preventDefault();
+
+      if (total < 30) {
+        $("#Java").fadeToggle(750);
+      }
+      else if (total < 60) {
+        $("#C").fadeToggle(750);
+          return
+      }
+      else if (total < 90) {
+        $("#Ruby").fadeToggle(750);
+          return
+      }
+      else if (total > 89) {
+        $("#PHP").fadeToggle(750);
+          return
+      }
+  });
+
+
+
     // //
-    //     if (total < 4) {
-    //       $("#show1").fadeToggle(750);
-    //     }
+
     //
     //
     //
     //
     //
+    // var valueQ1 = parseInt($("#Q1").val());
+    //     total += valueQ1
+    //         alert(total);
     //
-    //
+    //   event.preventDefault();
+    // //
     //     $(document).ready(function() {
     //       $("#formOne").submit(function(event) {
     //         var shoutInput = $("input#shout").val();
