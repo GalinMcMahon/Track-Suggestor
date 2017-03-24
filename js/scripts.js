c51febc09bc247ac98dd11df3df279aa5b4a4398
Galin McMahon <galinmcmahon@gmail.com>
2017-03-24 15:45:00 -0700

    Alter layout for time constraint.  Complete branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,49 @@
 $(document).ready(function(event){
 
     $(".goButton").click(function(event){
-      $(".monitor").hide();
       $(".goButton").hide();
       $(".Questions").show(1000);
   });
 
   var total = 0;
+  $("#Question1").show();
   $(".enterButton").click(function(event) {
-      $("#Question1").fadeToggle(750);
-
+    $("#Question1").hide();
+    
       var valueQ1 = parseInt($("#Q1").val());
       total += valueQ1
-        alert(total);
 
-});
+      var valueQ2 = parseInt($("#Q2").val());
+      total += valueQ2
 
-//   $(".enterButton2").click(function(event) {
-//         $("#Q2").show(1000);
-//       $("#Question2").fadeToggle(750);
-//
-//       var valueQ2 = parseInt($("#Q2").val());
-//       total += valueQ2
-// });
+      var valueQ3 = parseInt($("#Q3").val());
+      total += valueQ3
 
+      var valueQ4 = parseInt($("#Q4").val());
+      total += valueQ4
 
+      var valueQ5 = parseInt($("#Q5").val());
+      total += valueQ5
       event.preventDefault();
 
       if (total < 30) {
-        $("#Java").fadeToggle(750);
+        $("#Java").show();
       }
+
       else if (total < 60) {
-        $("#C").fadeToggle(750);
+        $("#C").show();
           return
       }
       else if (total < 90) {
-        $("#Ruby").fadeToggle(750);
+        $("#Ruby").show();
           return
       }
       else if (total > 89) {
-        $("#PHP").fadeToggle(750);
+        $("#PHP").show();
           return
       }
   });
+});
 
 
 
@@ -67,6 +68,6 @@ $(document).ready(function(event){
     //
     //         // $("#caps").show();
     //
-    //         event.preventDefault();
+
     //       });
     //     });
